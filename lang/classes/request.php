6e8560086741d24c66ac2f7b59eb36ca33d31452
Ed3 <ll_edmond@walla.com>
2013-12-29 17:51:35 +0200

    Create request.php

---
+++ lang/classes/request.php
@@ -0,0 +1,22 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+class Request extends Kohana_Request {
+	static public $lang = '';
+	public static function factory($uri = TRUE, HTTP_Cache $cache = NULL, $injected_routes = array()) {
+		if($uri === TRUE) {
+			$uri = Request::detect_uri();
+		}
+		$uri = ltrim($uri, '/');
+		$cfg = Kohana::$config->load('lang');
+		preg_match('~^(?:'.implode('|', array_keys($cfg['languages'])).')(?=/|$)~i', $uri, $matches);
+		if(empty($matches)){
+			I18n::$lang = $cfg['languages'][$cfg['default']]['i18n_code'];
+			setlocale(LC_ALL, $cfg['languages'][$cfg['default']]['locale']);
+			return parent::factory($cfg['default'].'/'.$uri, $cache);
+		}
+		Request::$lang = strtolower(arr::get($matches,0,$cfg['default']));
+		I18n::$lang = $cfg['languages'][Request::$lang]['i18n_code'];
+		setlocale(LC_ALL, $cfg['languages'][Request::$lang]['locale']);
+		$uri = preg_replace("/^".Request::$lang."\/|^".Request::$lang."$/", '\\0', $uri);
+		return parent::factory($uri, $cache, $injected_routes);
+	}
+}
