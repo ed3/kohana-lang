8697c023f9b8b80a7852e5048d81ce79432539eb
Ed3 <ll_edmond@walla.com>
2013-12-29 17:54:12 +0200

    Create lang.php

---
+++ lang/config/lang.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+return array(
+	'default'=>'en',
+	'languages'=>array(
+		'en'=>array(
+			'i18n_code'=> 'en_US',
+			'locale'=> array('en_US.utf-8'),
+			'name'=> 'english',
+		),
+		// add more languages
+	)
+);
