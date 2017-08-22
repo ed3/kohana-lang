ee3b86737288371a198a726f41e73f69912ce0e2
Ed <ed3@users.noreply.github.com>
2017-08-22 15:03:02 +0300

    koseven 3.3.x
    -add subdomain

--- classes/URL.php
+++ classes/URL.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 class URL extends Kohana_URL {
-	public static function site($uri = '', $protocol = FALSE, $lang = TRUE) {
+	public static function site($uri = '', $protocol = FALSE, $lang = TRUE, $subdomain = NULL) {
 		if ($lang === TRUE) {
 			$uri = Request::$lang.'/'.ltrim($uri, '/');
 		} elseif (is_string($lang)) {
